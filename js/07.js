b0e0d74c404121a7d8fe5a128c24e99416b241b5
jael <jj0110pz@gmail.com>
2023-05-31 00:01:07 -0700

    correos

---
+++ js/07.js
@@ -0,0 +1,19 @@
+//orden de las operaciones
+let resultado;
+
+resultado = (20 + 30) * 2;
+
+resultado = (600 + 600) * 1.05
+
+//console.log(resultado);
+
+//incrementos
+
+let puntaje = 10;
+
+//incrementos en 1 ++
+puntaje += 10;
+
+
+
+console.log(puntaje);
---
+++ js/08.js
@@ -0,0 +1,10 @@
+const nombre = "jael";
+const email = "correo@correo.com";
+
+//concatenacion
+
+console.log("nombre cliente: " + nombre + " email: " + email);
+
+//template strings - string literals
+
+console.log(`nombre cliente: ${nombre} email ${email}`);
